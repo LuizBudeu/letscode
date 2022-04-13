68048e3c6acad61a8f11a6cd686abc513d6276cd
Luiz Budeu <lugui@usp.br>
2022-04-12 22:21:45 -0300

    atualizando

---
+++ exemplos/src/index2.js
@@ -0,0 +1,18 @@
+import express from 'express';
+/* import productRouter from "./routes/products.js";
+import streamRouter from "./routes/stream.js";
+import canalRouter from "./routes/canal.js"; */
+import { productRouter, streamRouter, canalRouter } from '../../projetinho/src/routes/index.js';
+import { PORT } from '../../projetinho/src/constants.js';
+
+const server = express();
+
+server.use(express.json());
+server.use('/products/', productRouter);
+server.use('/stream/', streamRouter);
+server.use('/canal/', canalRouter);
+
+
+server.listen(PORT, () => { // 3000 é a porta, acessar 'localhost:3000/'
+  console.log(`Estou pronto em desenvolvimento, na porta ${PORT}`);
+});
